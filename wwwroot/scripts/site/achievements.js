c16fb7553e1b7b0a50f5efdecc4f2a29e27b2ad7
jesspring <jesswale94@gmail.com>
2024-09-21 22:17:11 +0100

    Remove testing code

--- wwwroot/scripts/site/achievements.js
+++ wwwroot/scripts/site/achievements.js
@@ -36,35 +36,6 @@ const achievementInfo = [
     },
 ];
 
-//For testing
-var testingCode = 0;
-var testingEnabled = false;
-window.addEventListener("keydown", (event) => {
-    if (event.key == "Pause")
-        testingCode++;
-    else
-        testingCode = 0;
-
-    if (testingCode == 3 && !testingEnabled) {
-        testingEnabled = true;
-        console.log("Testing mode enabled");
-    }
-
-    if (testingEnabled) {
-        if (event.key == "p") {
-            unlockAchievement("number_one_fan");
-        }
-
-        if (event.key == "c") {
-            window.localStorage.clear();
-        }
-
-        if (event.key == "s") {
-            console.log(window.localStorage);
-        }
-    }
-});
-
 function unlockAchievement(achievement) {
     //Try to get achievement from storage
     const achievementId = achievementIdMap[achievement];
