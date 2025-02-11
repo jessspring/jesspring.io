41314bae99bb698cf2d795148d729f311112a596
jesspring <jesswale94@gmail.com>
2025-02-11 23:09:17 +0000

    Improve the seasons and episodes lists

--- wwwroot/scripts/jessflix/watchTv.js
+++ wwwroot/scripts/jessflix/watchTv.js
@@ -27,11 +27,35 @@ fetch(tvUrl.replace("{tvId}", window.data.tvId))
         document.getElementById("title-description").innerHTML = json.overview;
 
         //Set season select options
+        //Remove "Specials" season and add to the end of the list later
+        let seasons = json.seasons;
+        let specials = null;
+        if (seasons[0].season_number == 0) {
+            specials = seasons[0];
+            specials.full_name = "Specials";
+            seasons = seasons.slice(1);
+        }
+
+        //If all of the seasons are called "Season X" or "Series X" don't add "Season X: " before their name
+        let allNamesMatch = true;
+        for (const season of seasons) {
+            season.full_name = `Season ${season.season_number}: ${season.name}`;
+            season.series_name = `Series ${season.season_number}`;
+
+            if (!(season.full_name.startsWith(season.name) || season.series_name == season.name))
+                allNamesMatch = false;
+        }
+
+        //Add specials if it was removed
+        if (specials != null)
+            seasons.push(specials);
+
+        //Add option for each season
         seasonSelect.innerHTML = "";
-        for (const season of json.seasons) {
+        for (const season of seasons) {
             const optionElement = document.createElement("option");
             optionElement.value = season.season_number;
-            optionElement.text = `Season ${season.season_number}: ${season.name}`;
+            optionElement.text = allNamesMatch ? season.name : season.full_name;
             seasonSelect.appendChild(optionElement);
         }
 
@@ -46,11 +70,21 @@ function loadEpisodes() {
         .then(x => x.json())
         .then(json => {
             //Set episode select options
+            //If all of the episodes are called "Episode X" don't add "Episode X: " before their name
+            let allNamesMatch = true;
+            for (const episode of json.episodes) {
+                episode.full_name = `Episode ${episode.episode_number}: ${episode.name}`;
+
+                if (!episode.full_name.startsWith(episode.name))
+                    allNamesMatch = false;
+            }
+
+            //Add option for each episode
             episodeSelect.innerHTML = "";
             for (const episode of json.episodes) {
                 const optionElement = document.createElement("option");
                 optionElement.value = episode.episode_number;
-                optionElement.text = `Episode ${episode.episode_number}: ${episode.name}`;
+                optionElement.text = allNamesMatch ? episode.name : episode.full_name;
                 episodeSelect.appendChild(optionElement);
             }
 
