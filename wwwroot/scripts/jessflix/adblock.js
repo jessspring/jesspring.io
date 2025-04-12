188e351b03144e79462389ce4ba0971f4a877cae
jesspring <jesswale94@gmail.com>
2025-04-12 22:11:51 +0100

    Add adblocker message to Jessflix

---
+++ wwwroot/scripts/jessflix/adblock.js
@@ -0,0 +1,10 @@
+﻿const adblockContainer = document.getElementById("adblock-container");
+const adblockButton = document.getElementById("adblock-button");
+
+if (window.localStorage.getItem("adblock-hidden") !== "true")
+    adblockContainer.classList.toggle("display-none", false);
+
+adblockButton.addEventListener("click", () => {
+    window.localStorage.setItem("adblock-hidden", "true");
+    adblockContainer.classList.toggle("display-none", true);
+});
