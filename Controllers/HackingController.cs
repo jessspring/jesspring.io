502a65e20edf8d09000b8bed235695f58e849f66
jesspring <jesswale94@gmail.com>
2026-02-14 01:11:37 +0000

    Initial Fallout minigame

---
+++ Controllers/HackingController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace jesspring.io.Controllers
+{
+    public class HackingController : BaseController
+    {
+        public static string Name => GetControllerName(nameof(HackingController));
+
+        [Route("/cVGDP2ia41JH7fGBTIdRnfOJ")]
+        public IActionResult Fallout()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/scripts/hacking/fallout.js
@@ -0,0 +1,370 @@
+﻿const colours = {
+    black: "#000000",
+    magenta: "#ff00ff",
+    yellow: "#ffff00"
+};
+
+const ySpacing = 24;
+const xSpacing = 14;
+const fontSize = 20;
+const fontType = "Lucida Console";
+const font = `${fontSize}px ${fontType}`;
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+context.imageSmoothingEnabled = false;
+
+window.addEventListener("resize", canvasResize);
+canvasResize();
+
+class Mouse {
+    x = -1;
+    y = -1;
+
+    previousMouseButtonDown = false;
+    currentMouseButtonDown = false;
+    runningMouseButtonDown = false;
+
+    constructor() {
+        window.addEventListener("mousemove", event => {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        });
+
+        window.addEventListener("mousedown", event => {
+            if (event.button == 0)
+                this.runningMouseButtonDown = true;
+        });
+
+        window.addEventListener("mouseup", event => {
+            if (event.button == 0)
+                this.runningMouseButtonDown = false;
+        });
+    }
+
+    update() {
+        this.previousMouseButtonDown = this.currentMouseButtonDown;
+        this.currentMouseButtonDown = this.runningMouseButtonDown;
+    }
+
+    isClicked() {
+        return this.currentMouseButtonDown && !this.previousMouseButtonDown;
+    }
+}
+
+function canvasResize() {
+    canvas.style.width = window.innerWidth;
+    canvas.style.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    context.imageSmoothingEnabled = false;
+}
+
+let previousTimestamp = 0;
+let delta = 0;
+const mouse = new Mouse();
+
+
+//53 x 22
+let attempts = 4;
+const hexStart = 62704;
+const hexStep = 62716 - hexStart;
+const symbols = [..."(){}[]<>!?@;:#-+=,_$/\\.'\"%*|^"];
+let puzzleText = "";
+const usedBracketIndices = [];
+let log = [];
+let password = "";
+
+for (let i = 0; i < 12 * 34; i++)
+    puzzleText += getRandomSymbol();
+
+let lines = [];
+let allowUpdate = true;
+
+fetch("https://raw.githubusercontent.com/powerlanguage/word-lists/refs/heads/master/common-7-letter-words.txt")
+    .then(x => x.text())
+    .then(text => {
+        let words = text.split(String.fromCharCode(10))
+            .map(x => x.toUpperCase().substring(0, 7));
+        words.push("JESSICA");
+
+        const wordPositions = [];
+        for (let i = 0; i < 20; i++)
+            while (true) {
+                const randomPos = randomInt((12 * 34) - 6);
+
+                const overlap = wordPositions.some(x => randomPos + 7 >= x && randomPos <= x + 7);
+                if (!overlap) {
+                    wordPositions.push(randomPos);
+                    break;
+                }
+            }
+
+        wordPositions.forEach(x => {
+            const word = words[randomInt(words.length)];
+            puzzleText = puzzleText.substring(0, x) + word + puzzleText.substring(x + 7);
+            words = words.filter(x => x != word);
+            password = word;
+        });
+
+        updateLines();
+
+        run();
+    });
+
+function run() {
+    previousTimestamp = document.timeline.currentTime;
+    requestAnimationFrame(update);
+}
+
+function update(timestamp) {
+    delta = (timestamp - previousTimestamp) / 1000;
+    previousTimestamp = timestamp;
+
+    context.clearRect(-Number.MAX_SAFE_INTEGER / 2, -Number.MAX_SAFE_INTEGER / 2, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+
+    mouse.update();
+
+    drawStaticText();
+    drawLines();
+    drawLog();
+    drawMouse();
+
+    handleLink();
+
+    requestAnimationFrame(update);
+
+    if (!allowUpdate)
+        return;
+
+    handleInput();
+}
+
+function handleLink() {
+    if (mouse.x < 1 ||
+        mouse.x > 10 * xSpacing ||
+        mouse.y < 1 ||
+        mouse.y > 1 * ySpacing)
+        return;
+
+    drawText("JESSPRING", 0, 0, colours.yellow, colours.magenta);
+
+    if (!mouse.isClicked())
+        return;
+
+    window.location.href = "https://jesspring.io";
+}
+
+function handleInput() {
+    if (!mouse.isClicked())
+        return;
+
+    const mouseIndex = getMouseIndex();
+    if (mouseIndex == -1)
+        return;
+
+    if (openBracketChars.includes(puzzleText[mouseIndex]))
+        handleBracketInput(mouseIndex)
+
+    if (!symbols.includes(puzzleText[mouseIndex]))
+        handleLetterInput(mouseIndex);
+}
+
+function handleBracketInput(mouseIndex) {
+    if (usedBracketIndices.includes(mouseIndex))
+        return;
+
+    const range = getMousedOpenBracketRange(mouseIndex);
+    if (range[0] == range[1])
+        return;
+
+    let word = "";
+    for (let i = range[0]; i <= range[1]; i++)
+        word += puzzleText[i];
+
+    addToLog(word);
+    addToLog("Attempt added");
+
+    attempts++;
+    usedBracketIndices.push(mouseIndex);
+}
+
+function handleLetterInput(mouseIndex) {
+    const range = getMousedLetterRange(mouseIndex);
+    let word = "";
+    for (let i = range[0]; i <= range[1]; i++)
+        word += puzzleText[i];
+
+    addToLog(word);
+
+    if (word == password) {
+        addToLog("Exact match!");
+        addToLog("Please wait");
+        addToLog("while system");
+        addToLog("is accessed.");
+
+        allowUpdate = false;
+
+        setTimeout(() => window.location.href = "https://www.google.com", 3000);
+    }
+    else if (attempts == 1)
+        window.location.href = "https://jesspring.io";
+    else {
+        addToLog("Entry denied");
+        attempts--;
+
+        let matchingLetters = 0;
+        for (let i = 0; i < 7; i++)
+            if (password[i] == word[i])
+                matchingLetters++;
+
+        addToLog(`${matchingLetters}/7 correct.`);
+    }
+}
+
+function addToLog(string) {
+    log = [string].concat(log);
+    if (log.length > 15)
+        log = log.slice(0, 15);
+}
+
+function drawCharacter(char, x, y, textColour = colours.magenta, backgroundColour = colours.black) {
+    context.fillStyle = backgroundColour;
+    context.fillRect(x * xSpacing, y * ySpacing, xSpacing, ySpacing);
+    context.font = font;
+    context.textBaseline = "top";
+    context.fillStyle = textColour;
+    context.fillText(char, x * xSpacing, y * ySpacing);
+}
+
+function drawText(text, x, y, textColour = colours.magenta, backgroundColour = colours.black) {
+    [...text].forEach(c => {
+        drawCharacter(c, x, y, textColour, backgroundColour);
+        x++;
+    });
+}
+
+function drawStaticText() {
+    drawText("JESSPRING INDUSTRIES TERMLINK PROTOCOL", 0, 0);
+
+    if (attempts == 1)
+        drawText("!!! WARNING: LOCKOUT IMMINENT !!!", 0, 1);
+    else
+        drawText("ENTER PASSWORD NOW", 0, 1);
+
+    drawText(`${attempts} ATTEMPT(S) LEFT:` + " █".repeat(attempts), 0, 3);
+
+    for (let i = 0; i < 34; i++)
+        drawText(`0x${(hexStart + (hexStep * i)).toString(16).toUpperCase()}`, Math.floor(i / 17) * 20, 5 + i % 17);
+}
+
+function drawLines() {
+    for (let i = 0; i < 34; i++)
+        drawText(lines[i], 7 + Math.floor(i / 17) * 20, 5 + i % 17);
+}
+
+function getMouseIndex() {
+    if (mouse.x > 7 * xSpacing &&
+        mouse.x < 19 * xSpacing &&
+        mouse.y > 5 * ySpacing &&
+        mouse.y < 22 * ySpacing)
+        return ((Math.floor(mouse.y / ySpacing) - 5) * 12) + (Math.floor(mouse.x / xSpacing) - 7);
+
+    if (mouse.x > 27 * xSpacing &&
+        mouse.x < 39 * xSpacing &&
+        mouse.y > 5 * ySpacing &&
+        mouse.y < 22 * ySpacing)
+        return ((Math.floor(mouse.y / ySpacing) + 12) * 12) + (Math.floor(mouse.x / xSpacing) - 27);
+
+    return -1;
+}
+
+function drawMouse() {
+    const mouseIndex = getMouseIndex();
+    if (mouseIndex == -1) {
+        drawCharacter("█", 41, 21);
+        return;
+    }
+
+    const range = getMousedRange(mouseIndex);
+
+    for (let i = range[0]; i <= range[1]; i++) {
+        drawCharacter(puzzleText[i], 41 + (i - range[0]), 21, colours.magenta, colours.black);
+        drawCharacter(puzzleText[i],
+            7 + (i % 12) + (Math.floor(i / (12 * 17)) * 20),
+            5 + Math.floor(i / 12) - (Math.floor(i / (12 * 17)) * 17),
+            colours.black, colours.yellow);
+    }
+}
+
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
+
+function getMousedRange(mouseIndex) {
+    const mousedChar = puzzleText[mouseIndex];
+
+    if (openBracketChars.includes(mousedChar))
+        return getMousedOpenBracketRange(mouseIndex);
+
+    if (!symbols.includes(mousedChar))
+        return getMousedLetterRange(mouseIndex);
+
+    return [mouseIndex, mouseIndex];
+}
+
+const openBracketChars = [..."({[<"];
+const bracketPairChars = [
+    ["(", ")"],
+    ["{", "}"],
+    ["[", "]"],
+    ["<", ">"],
+];
+function getMousedOpenBracketRange(mouseIndex) {
+    if (usedBracketIndices.includes(mouseIndex))
+        return [mouseIndex, mouseIndex];
+
+    const closedBracket = bracketPairChars.filter(x => x[0] == puzzleText[mouseIndex])[0][1];
+
+    for (let i = mouseIndex + 1; i % 12 != 0; i++) {
+        if (!symbols.includes(puzzleText[i]))
+            return [mouseIndex, mouseIndex]
+
+        if (puzzleText[i] == closedBracket)
+            return [mouseIndex, i];
+    }
+
+    return [mouseIndex, mouseIndex];
+}
+
+function getMousedLetterRange(mouseIndex) {
+    const range = [mouseIndex, mouseIndex];
+
+    while (range[0] >= 0 && !symbols.includes(puzzleText[range[0]]))
+        range[0]--;
+
+    while (range[1] < 12 * 34 && !symbols.includes(puzzleText[range[1]]))
+        range[1]++;
+
+    range[0]++;
+    range[1]--;
+
+    return range;
+}
+
+function getRandomSymbol() {
+    return symbols[randomInt(symbols.length)];
+}
+
+function updateLines() {
+    lines = [];
+    for (let i = 0; i < 34; i++)
+        lines.push(puzzleText.substring(i * 12, (i + 1) * 12));
+}
+
+function drawLog() {
+    for (let i = 0; i < log.length; i++)
+        drawText(">" + log[i], 40, 19 - i);
+
+    drawCharacter(">", 40, 21);
+}
